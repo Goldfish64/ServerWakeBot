9c818145d23d458e3ec37ddfe389112c997dbb96
John Davis <goldfishnet@aol.com>
2018-07-15 22:24:05 -0500

    Initial commit

---
+++ ServerWakeBot/Commands.cs
@@ -0,0 +1,168 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* File: Commands.cs
+* 
+* Copyright (c) 2018 John Davis
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+* IN THE SOFTWARE.
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using Discord.Commands;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ServerWakeBot {
+    public sealed class CommandModule : ModuleBase {
+        private bool TestMac(string macAddress) {
+            return Regex.IsMatch(macAddress, "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$");
+        }
+
+        /// <summary>
+        /// Tests a MAC address.
+        /// </summary>
+        [Command("testmac")]
+        private async Task TestMacAsync(string macAddress) {
+            if (TestMac(macAddress))
+                await ReplyAsync($"MAC address `{macAddress.ToUpperInvariant()}` is valid!");
+            else
+                await ReplyAsync($"MAC address `{macAddress.ToUpperInvariant()}` is not valid.");
+        }
+
+        /// <summary>
+        /// Adds a MAC address.
+        /// </summary>
+        [Command("addmac")]
+        private async Task AddMacAsync(string name, string macAddress) {
+            if (!TestMac(macAddress)) {
+                await ReplyAsync($"MAC address `{macAddress.ToUpperInvariant()}` is not valid.");
+                return;
+            }
+
+            // Get dictionary for guild.
+            var dict = new Dictionary<string, string>();
+            if (File.Exists(Context.Guild.Id.ToString() + Program.MacsFileName))
+                dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(Context.Guild.Id.ToString() + Program.MacsFileName));
+
+            // Add to dictionary.
+            dict[name] = macAddress.ToUpperInvariant();
+
+            // Save settings.
+            File.WriteAllText(Context.Guild.Id.ToString() + Program.MacsFileName, JsonConvert.SerializeObject(dict));
+            await ReplyAsync($"MAC address `{macAddress.ToUpperInvariant()}` added for `{name}`.");
+        }
+
+        /// <summary>
+        /// Deletes a MAC address.
+        /// </summary>
+        [Command("delmac")]
+        private async Task DelMacAsync(string name) {
+            // Get dictionary for guild.
+            var dict = new Dictionary<string, string>();
+            if (File.Exists(Context.Guild.Id.ToString() + Program.MacsFileName))
+                dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(Context.Guild.Id.ToString() + Program.MacsFileName));
+
+            // Delete from dictionary.
+            dict.Remove(name);
+
+            // Save settings.
+            File.WriteAllText(Context.Guild.Id.ToString() + Program.MacsFileName, JsonConvert.SerializeObject(dict));
+            await ReplyAsync($"MAC address for `{name}` is deleted.");
+        }
+
+        /// <summary>
+        /// Gets a MAC address.
+        /// </summary>
+        [Command("getmac")]
+        private async Task GetMacAsync(string name) {
+            // Get dictionary for guild.
+            var dict = new Dictionary<string, string>();
+            if (File.Exists(Context.Guild.Id.ToString() + Program.MacsFileName))
+                dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(Context.Guild.Id.ToString() + Program.MacsFileName));
+
+            // Get from dictionary.
+            string macAddress;
+            if (!dict.TryGetValue(name, out macAddress)) {
+                await ReplyAsync($"No MAC exists for `{name}`.");
+                return;
+            }
+
+            await ReplyAsync($"MAC address for `{name}` is `{macAddress.ToUpperInvariant()}`.");
+        }
+
+        /// <summary>
+        /// Wakes a MAC address.
+        /// </summary>
+        [Command("wakemac")]
+        private async Task WakeMacAsync(string name) {
+            // Get dictionary for guild.
+            var dict = new Dictionary<string, string>();
+            if (File.Exists(Context.Guild.Id.ToString() + Program.MacsFileName))
+                dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(Context.Guild.Id.ToString() + Program.MacsFileName));
+
+            // Get from dictionary.
+            string macAddress;
+            if (!dict.TryGetValue(name, out macAddress)) {
+                await ReplyAsync($"No MAC exists for `{name}`.");
+                return;
+            }
+
+            await ReplyAsync($"Waking up host `{macAddress.ToUpperInvariant()}`...");
+
+            // https://www.codeproject.com/Articles/5315/Wake-On-Lan-sample-for-C.
+            // Create UdpClient.
+            var wolClient = new UdpClient();
+
+            // Create buffer.
+            int counter = 0;
+            var bytes = new byte[1024];
+
+            // First 6 bytes are 0xFF.
+            for (int i = 0; i < 6; i++)
+                bytes[counter++] = 0xFF;
+
+            // Repeate MAC 16 times.
+            for (int i = 0; i < 16; i++) {
+                int z = 0;
+                for (int j = 0; j < 6; j++) {
+                    bytes[counter++] = byte.Parse(macAddress.Substring(z, 2), NumberStyles.HexNumber);
+                    z += 3;
+                }
+            }
+
+            // Print to console.
+            int zz = 0;
+            for (int i = 0; i < 200; i++) {
+                Console.Write(bytes[i] + " ");
+                zz++;
+                if (zz >= 6) {
+                    Console.WriteLine();
+                    zz = 0;
+                }
+            }
+
+            wolClient.Send(bytes, bytes.Length, new IPEndPoint(IPAddress.Parse(Program.BroadcastMac), 0));
+        }
+    }
+}
--- ServerWakeBot/Program.cs
+++ ServerWakeBot/Program.cs
@@ -1,12 +1,104 @@
-﻿using System;
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* File: Program.cs
+* 
+* Copyright (c) 2018 John Davis
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+* IN THE SOFTWARE.
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-namespace ServerWakeBot
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ServerWakeBot {
+    public class Program {
+        #region Constants
+
+        public const string TokenFileName = "token.txt";
+        public const string MacsFileName = "-macs.json";
+        public const string CommandPrefix = "%";
+
+        public const string BroadcastMac = "192.168.1.255"; // Replace with broadcast address for local subnet.
+
+        #endregion
+
+        public static void Main(string[] args) => RunBot().GetAwaiter().GetResult();
+
+        private static async Task RunBot() {
+            // Ensure token exists.
+            if (!File.Exists(TokenFileName)) {
+                Console.WriteLine("Please place bot token in a file called \"token.txt\". Press any key to exit...");
+                Console.ReadKey();
+                return;
+            }
+
+            // Read token from file.
+            var token = File.ReadAllText(TokenFileName);
+
+            // Create Discord client.
+            var client = new DiscordSocketClient();
+            client.Log += (e) => {
+                Console.WriteLine(e.ToString());
+                return Task.CompletedTask;
+            };
+
+            // Create command service and map.
+            var commands = new CommandService();
+            var commandMap = new ServiceCollection();
+
+            // Load commands from assembly.
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            // Listen for messages.
+            client.MessageReceived += async (message) => {
+                // Get the message and check to see if it is a user message.
+                var msg = message as IUserMessage;
+                if (msg == null)
+                    return;
+
+                // Keeps track of where the command begins.
+                var pos = 0;
+
+                // Attempt to parse a command.
+                if (msg.HasStringPrefixLower(CommandPrefix, ref pos)) {
+                    var result = await commands.ExecuteAsync(new CommandContext(client, msg), msg.Content.Substring(pos));
+                    if (!result.IsSuccess) {
+                        // Is the command just unknown? If so, return.
+                        if (result.Error == CommandError.UnknownCommand)
+                            return;
+
+                        await msg.Channel.SendMessageAsync($"Error: {result.ErrorReason}\n\nIf there are spaces in a parameter, make sure to surround it with quotes.");
+                    }
+                    return;
+                }
+            };
+
+            // Login to Discord.
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
         }
     }
 }
